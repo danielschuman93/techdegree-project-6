f4603ff66e08139250ffee3816b255cbb9c9944a
Daniel Schuman <making_melodies93@yahoo.com>
2020-05-27 17:24:03 -0700

    Add routes / Update pug templates

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const data = require('data.json');
+const { projects } = require('./data.json');
 
 const app = express();
 
@@ -8,8 +8,16 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
-})
+    res.render('index', { projects });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/projects/:id', (req, res) => {
+    res.render('project');
+});
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
